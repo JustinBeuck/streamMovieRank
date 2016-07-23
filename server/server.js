2fb197f346d295f0e87e1a445154d070d1898f3e
Justin Beuck <jbeuck@yahoo.com>
2016-07-22 21:36:02 -0500

    added server.js file inside of server folder...added express and webpack to server.js

--- server/server.js
+++ server/server.js
@@ -1,10 +1,27 @@
+var path = require('path');
 var express = require('express');
-var app = express();
+var webpack = require('webpack');
+var config = require('./webpack.config.dev');
 
-app.get('/', function (req, res) {
-  res.send('Home of future react app!');
+var app = express();
+var compiler = webpack(config);
+
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
+
+app.use(require('webpack-hot-middleware')(compiler));
+
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(3000, function () {
-  console.log('listening on port 3000...');
+app.listen(8000, 'localhost', function(err) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  console.log('Listening at http://localhost:8000');
 });
