38f87783ab598793d3d9799497aaffd9ab0a78a7
Justin Beuck <jbeuck@yahoo.com>
2016-07-22 22:10:02 -0500

    added babelrc and eslintrc

---
+++ client/components/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Main = React.createClass({
+	render() {
+		return (
+			<div>
+				<h1>Kelly Jo</h1>
+			</div>
+		)
+	}
+});
+
+render(router, document.getElementById('root'));
---
+++ client/streamMovieRank.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { render } from 'react-dom';
+
+const streamMovieRank = React.createClass({
+	render() {
+		return (
+			<div>
+				<h1>Kelly Jo</h1>
+			</div>
+		)
+	}
+});
+
+render(<streamMovieRank />, document.getElementById('root'));
